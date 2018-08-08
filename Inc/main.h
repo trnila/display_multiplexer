521a4afa103a925f6c90ca8a49e59347a0c60d10
Daniel Trnka <daniel.trnka@gmail.com>
2018-08-08 23:53:34 +0200

    Oled: added ssd1306 library

--- Inc/main.h
+++ Inc/main.h
@@ -65,6 +65,12 @@
 #define TFT_RST_GPIO_Port GPIOC
 #define TFT_CS_Pin GPIO_PIN_15
 #define TFT_CS_GPIO_Port GPIOC
+#define SSD1306_Reset_Pin GPIO_PIN_0
+#define SSD1306_Reset_GPIO_Port GPIOA
+#define SSD1306_DC_Pin GPIO_PIN_1
+#define SSD1306_DC_GPIO_Port GPIOA
+#define SSD1306_CS_Pin GPIO_PIN_2
+#define SSD1306_CS_GPIO_Port GPIOA
 
 /* ########################## Assert Selection ############################## */
 /**
--- Inc/utils.h
+++ Inc/utils.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #define HALT() asm("bkpt #1"); for(;;);
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
